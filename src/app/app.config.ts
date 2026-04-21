4bd10bbfd2117084e0b6134072b625df1104b092
XimenesJu <ju.cx@hotmail.com>
2026-04-21 20:35:01 -0300

    chore: initial setup, models and zone.js installation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http'; // Ajuste 1: Importar
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideHttpClient() // Ajuste 2: Adicionar aqui
   ]
 };
